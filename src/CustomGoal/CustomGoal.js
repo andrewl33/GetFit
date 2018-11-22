6d1d7242bf76b2e61caa81c46bdb2d81f6f92b26
Hamilton <hamiltonskrabanek@gmail.com>
2018-11-21 22:08:59 -0600

    flesh out object paramaters

--- src/CustomGoal/CustomGoal.js
+++ src/CustomGoal/CustomGoal.js
@@ -5,33 +5,83 @@ export default class SO_LoginExample extends Component<{}> {
     constructor(props) {
         super(props);
         this.state = {
-            email: undefined,
-            password: undefined, 
+        	id: undefined,
+            title: undefined,
+            createDate: undefined, 
+            endDate: undefined,
+            isActive: undefined,
+            unitSteps: undefined,
+            startAmount: undefined,
+            progress: undefined,
+            endAmount: undefined
         }
     }
 
-    login() {
-        console.log(this.state.email, this.state.password);
+    submit() {
+        console.log(this.state.id, this.state.title, this.state.createDate, this.state.endDate, this.state.isActive, this.state.unitSteps, this.state.startAmount, this.state.progress, this.state.endAmount);
     }
 
     render() {
         return(
             <View>
-            	<Text>Title</Text>
+            	<Text>Crate A Custom Goal</Text>
                 <TextInput
                     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                    onChangeText={(inputVal) => this.setState({email: inputVal})}
-                value={this.state.email}
+                    onChangeText={(inputVal) => this.setState({id: inputVal})}
+                    placeholder="Id"
+                value={this.state.id}
                 />
                 <TextInput
                     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                    secureTextEntry={true}
-                    onChangeText={(inputVal) => this.setState({password: inputVal})}
-                value={this.state.password}
+                    onChangeText={(inputVal) => this.setState({title: inputVal})}
+                    placeholder="Title"
+                value={this.state.title}
                 />
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(inputVal) => this.setState({createdate: inputVal})}
+                    placeholder="Date Created"
+                value={this.state.createdate}
+                />
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(inputVal) => this.setState({endDate: inputVal})}
+                    placeholder="Date Ending"
+                value={this.state.endDate}
+                />
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(inputVal) => this.setState({isActive: inputVal})}
+                    placeholder="Is Active?"
+                value={this.state.isActive}
+                />                
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(inputVal) => this.setState({unitSteps: inputVal})}
+                    placeholder="Unit Steps"
+                value={this.state.unitSteps}
+                /> 
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(inputVal) => this.setState({startAmount: inputVal})}
+                    placeholder="Start Amount"
+                value={this.state.startAmount}
+                />
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(inputVal) => this.setState({progress: inputVal})}
+                    placeholder="Progress"
+                value={this.state.progress}
+                />
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(inputVal) => this.setState({endAmount: inputVal})}
+                    placeholder="End Amount"
+                value={this.state.endAmount}
+                />                                                                                                                              
                 <Button
-                    title="Login"
-                    onPress={() => this.login()}
+                    title="Submit"
+                    onPress={() => this.submit()}
                 />
             </View>
         )
